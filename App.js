2e8832b392f31daf12f494aa988e847503dc45a9
zerontec <zerontecnologia1@gmail.com>
2023-02-20 01:57:07 -0400

    update App file

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScren from './screens/HomeScren';
+
+export default function App() {
+  return (
+  
+     <HomeScren/>
+    
+  );
+}
