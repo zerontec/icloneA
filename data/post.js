3c255a1a8a1a1c6166dd5d8ab60561d646372180
zerontec <zerontecnologia1@gmail.com>
2023-02-20 02:00:55 -0400

    add data folder

---
+++ data/post.js
@@ -0,0 +1,106 @@
+import { USERS } from "./users";
+
+
+export const POSTS = [
+
+    {
+
+imageUrl:'https://image.com',
+user:USERS[0].user,
+likes: 7895,
+caption: 'Train Ride to Howarts',
+profile_picture:USERS[0].image,
+comments:[
+
+    {
+        user:'thegazman',
+        comment: 'Woo! this build looks',
+    },
+    {user:'amana.dev', 
+    comment: 'Woo! this build looks',
+
+    },
+    {
+        user:'thegazman',
+        comment: 'Woo! this build looks',
+    },
+
+
+
+
+
+
+]
+
+
+    },
+
+    {
+
+        imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+        ,
+        user:USERS[1].user,
+        likes: 7895,
+        caption: 'Train Ride to Howarts',
+        profile_picture:USERS[1].image,
+        comments:[
+        
+            {
+                user:'thegazman',
+                comment: 'Woo! this build looks',
+            },
+            {user:'amana.dev', 
+            comment: 'Woo! this build looks',
+        
+            },
+            {
+                user:'thegazman',
+                comment: 'Woo! this build looks',
+            },
+        
+        
+        
+        
+        
+        
+        ]
+        
+        
+            },
+            {
+
+                imageUrl:'https://image.com',
+                user:USERS[2].user,
+                likes: 7895,
+                caption: 'Train Ride to Howarts',
+                profile_picture:USERS[2].image,
+                comments:[
+                
+                    {
+                        user:'thegazman',
+                        comment: 'Woo! this build looks',
+                    },
+                    {user:'amana.dev', 
+                    comment: 'Woo! this build looks',
+                
+                    },
+                    {
+                        user:'thegazman',
+                        comment: 'Woo! this build looks',
+                    },
+                
+                
+                
+                
+                
+                
+                ]
+                
+                
+                    }
+                
+        
+
+
+
+]
---
+++ data/users.js
@@ -0,0 +1,151 @@
+export const USERS =[
+
+    {
+        user:'leonardo',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Dania',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    
+
+    {
+        user:'Amaia',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    
+
+    {
+        user:'leonardo',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    
+
+    {
+        user:'Hannah',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Wuilmaris',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Leoberto',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Lisbeth',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Johanna',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Hannah',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Hannah',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Hannah',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    {
+        user:'Hannah',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxSwOyhXLpfSxHvc32Ce_QbdzGqPh9wtJUnYFh4IsAw&s'
+
+
+
+
+
+    },
+    
+
+
+
+
+
+
+]
+
+// let data = USERS;
+
+// const cantidad=(data) => {
+// let res = 0;
+
+// for(let f = 0; f < data.length; f++){
+
+
+//     res++;
+
+// }
+
+// return res;
+
+// }
+
+// cantidad(data);
