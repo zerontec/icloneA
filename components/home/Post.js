519fc4f80886a6b7a9b50e7814efdd1202ca1ba2
zerontec <zerontecnologia1@gmail.com>
2023-02-23 00:13:53 -0400

    create footer component inside to PostComponet also add icons for the footer

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
 import {Divider} from 'react-native-elements'
+import { TouchableOpacity } from 'react-native';
 
 
 const Post = ({post}) => {
@@ -8,6 +9,10 @@ const Post = ({post}) => {
         <View style={{marginBottom:30}}>
             <Divider width={1} orientation='vertical' style={{color:'white'}} />
            <PostHeader post ={post} />
+           <PostImage post={post}/>
+           <View style={{marginHorizontal:15,marginTop:10}}>    
+           <PostFooter/>
+           </View>
         </View>
     );
 }
@@ -34,6 +39,72 @@ margin:5, alignItems:'center'}}>
 
 )
 
+const PostImage =({post}) =>(
+    <View style ={{width:'100', height:450,}}>
+
+<Image source ={{uri: post.imageUrl}} style={{height:'100%',
+    resizeMode: 'cover'}} />
+    </View>
+   
+
+)
+
+
+
+const PostFooter =() => (
+<View style={{flexDirection:'row'}}>
+    <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[0].imageUrl}/>
+    <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[0].imageUrl}/>
+    <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[0].imageUrl}/>
+    </View>
+
+
+
+)
+
+
+const Icon =({imgStyle,imgUrl}) => (
+
+<TouchableOpacity>
+
+<Image style={imgStyle} source={{uri:imgUrl}}/>
+
+</TouchableOpacity>
+
+)
+
+const postFooterIcons=[
+
+{
+name:'Like',
+imageUrl:'/assets/icons/icons8-heart-50.png',
+likeImageUrl:'https://',
+},
+
+{name:'Comment',
+   imageUrl: '/assets/icons/icons8-heart-50.png' 
+
+},
+{name:'share',
+   imageUrl: 'https://' 
+
+},
+
+{
+    name:'Save',
+   imageUrl: 'https://' 
+
+},
+
+]
+
+
+
+
+
+
+
+
 
 
 
@@ -49,6 +120,11 @@ const styles = StyleSheet.create({
         borderColor:'#ff8501'
 
     },
+    footerIcon:{
+        width:33,
+        height:33
+
+    }
 
 
 })
